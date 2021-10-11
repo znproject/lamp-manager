771c384e1021ab240cacade7f7ea7b2a53bd0bc5
Vitaliy <theyamshikov@yandex.ru>
2021-10-11 23:53:33 +0600

    upd

--- config/routing.php
+++ config/routing.php
@@ -2,9 +2,14 @@
 
 use Symfony\Component\Routing\Loader\Configurator\RoutingConfigurator;
 use ZnBundle\Dashboard\Symfony4\Web\Controllers\DashboardController;
+use ZnSandbox\Sandbox\Apache\Symfony4\Web\Controllers\ServerController;
 
 return function (RoutingConfigurator $routes) {
+    /*$routes
+        ->add('main_page', '/')
+        ->controller([DashboardController::class, 'index']);*/
+
     $routes
         ->add('main_page', '/')
-        ->controller([DashboardController::class, 'index']);
+        ->controller([ServerController::class, 'index']);
 };
